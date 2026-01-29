d3d5cfa27ca35e36bf18d6e8ce238654ca9b0467
Edward Liaw <ed.liaw@gmail.com>
2026-01-29 07:55:18 -0800

    Initial Commit

---
+++ Splitter/SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitterApp.swift
+//  Splitter
+//
+//  Created by Edward Liaw on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
